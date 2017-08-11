0eaed49b8a77f6ca761fc06840d30c3cda9983fa
xuqianghui <merlinkael@gmail.com>
2017-08-11 18:45:47 +0800

    zuul路由配置测试

---
+++ microservice-gateway-zuul/src/main/java/com/itmuch/config/PatternServiceRouteMapper.java
@@ -0,0 +1,12 @@
+package com.itmuch.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by leo on 2017/8/11.
+ */
+@Configuration
+public class PatternServiceRouteMapper {
+
+
+}
